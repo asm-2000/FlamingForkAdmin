60e6ea5093166455d2cd8ae2a891ee3d802df60b
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-22 13:47:33 +0545

    feat: implement title view in main page (logo,title,displaymode changer)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,24 +1,63 @@
-﻿using FlamingForkAdmin.Helper.Utilities;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FlamingForkAdmin.Helper.Utilities;
 using FlamingForkAdmin.Pages;
 
 namespace FlamingForkAdmin.ViewModels
 {
-    public partial class MainViewModel
+    public partial class MainViewModel : ObservableObject
     {
+        #region Properties
+
+        [ObservableProperty]
+        private string _DisplayModeButtonSource;
+
         private INavigation _Navigation;
+
+        #endregion Properties
+
+        #region Constructor
+
         public MainViewModel(INavigation navigation)
         {
+            DisplayModeButtonSource = Application.Current.UserAppTheme == AppTheme.Light ? "dark_mode_icon.png" : "light_mode_icon.png";
             _Navigation = navigation;
             CheckLoginStatus();
         }
 
+        #endregion Constructor
+
+        #region Methods
+
         public async Task CheckLoginStatus()
         {
             string? token = await SecureStorageHandler.GetAuthenticationToken();
-            if(token == "Not Found")
+            if (token == "Not Found")
             {
                 await _Navigation.PushModalAsync(new AdminLoginPage());
             }
         }
+
+        [RelayCommand]
+        public void ToggleDisplayMode()
+        {
+            Application.Current.UserAppTheme = Application.Current.UserAppTheme == AppTheme.Light ? AppTheme.Dark : AppTheme.Light;
+            DisplayModeButtonSource = Application.Current.UserAppTheme == AppTheme.Light ? "dark_mode_icon.png" : "light_mode_icon.png";
+        }
+
+        [RelayCommand]
+        public async Task LogOutAdmin()
+        {
+            // Clears all the saved data.
+            SecureStorageHandler.ClearStorage();
+            // Clears Shell's navigation stack.
+            Shell.Current.Items.Clear();
+            // Initialized the app to its initial state.
+            App.Current.MainPage = new AppShell();
+            // Opens Sign in page.
+            await _Navigation.PushModalAsync(new AdminLoginPage());
+        }
+
+        #endregion Methods
     }
 }
