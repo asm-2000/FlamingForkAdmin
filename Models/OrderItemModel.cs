c65e671f65d00285f1ea53080acd4f367e27515a
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-23 11:56:25 +0545

    feat: implement models necessary for representing orders

---
+++ Models/OrderItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FlamingForkAdmin.Models
+{
+    public class OrderItemModel
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public string? OrderItemName { get; set; }
+        public int OrderItemPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
