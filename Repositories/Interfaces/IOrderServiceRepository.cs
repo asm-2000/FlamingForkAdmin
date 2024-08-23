73a1afcebd28418f38a6daa52dd4b08cf1736ddf
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-23 14:40:48 +0545

    chore: UI modifications for picker

--- Repositories/Interfaces/IOrderServiceRepository.cs
+++ Repositories/Interfaces/IOrderServiceRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlamingForkAdmin.Repositories.Interfaces
+﻿namespace FlamingForkAdmin.Repositories.Interfaces
 {
-    internal interface IOrderServiceRepository
+    public interface IOrderServiceRepository
     {
     }
 }
