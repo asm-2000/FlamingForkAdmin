4805e7664830548a704d7aef241b10578d826963
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-25 23:01:03 +0545

    feat: implement a command parameter converter to pass a tuple as a command parameter to ChangeOrderStatus command

---
+++ Helper/Converters/TupleConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace FlamingForkAdmin.Helper.Converters
+{
+    public class TupleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is string orderId && values[1] != null)
+            {
+                return Tuple.Create(orderId, values[1].ToString());
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
